08adb564bf6d8a6b0b67db0a34ad6973ca0ef78a
Faysal Ahammed <faysal.csediu@gmail.com>
2023-06-20 11:01:58 +0600

    added custom query

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,9 +1,30 @@
+const Product = require('../models/product.model');
+
 const getAllProductsStatic = async (_req, res) => {
   res.status(200).json({ msg: 'product testing route' });
 };
 
-const getAllProducts = async (_req, res) => {
-  res.status(200).json({ msg: 'products route' });
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }; //regex
+  }
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
